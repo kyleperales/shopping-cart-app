84fc20b2eae8988ef6ff8c4215a645cbf3673c29
Kyle <kyleperalestudio@gmail.com>
2024-11-12 13:35:41 +0800

    initialize React app with Vite, add navigation bar, styles, and configuration files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from './components/navigation-bar/NavBar'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navigation-bar/NavBar.jsx
@@ -0,0 +1,16 @@
+import './NavBar.scss';
+
+function NavBar() {
+    return (
+        <div className='navbar'>
+            <div className=''>
+                Shoppee Clone
+            </div>
+            <div>
+                In cart: 0
+            </div>
+        </div>
+    );
+}
+
+export default NavBar
